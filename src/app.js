b4dbaf228740a7883ebd8e9b0a88de6e9d24fd76
Aman Upadhyay <146805438+amanupadhyay-obj@users.noreply.github.com>
2025-03-04 10:03:05 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -4,15 +4,45 @@ const connectDB=require("./config/database")
 exports.app = app;
 const User=require("./models/user");
 
-
+app.use(express.json());
 app.post("/signup", async (req,res) => {
-    const user = new User({
-        name: 'Khushi Sharma',
-        email: 'khushi@gmail.com'
-    })
+    user = new User(req.body);
     await user.save();
     res.send("User created successfully");
 })
+
+app.use(express.json());
+app.get("/user", async (req,res)=>{
+  const emailId = req.body.email;
+  try{
+    const user = await User.find({});
+    res.send(user);
+  } catch(err){
+      res.status(404).send("Something went wrong");
+  }
+});
+
+app.delete("/user" , async (req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId)
+        res.send("User is Deleted Successfully");
+    }
+    catch(err){
+        res.status(404).send("User not Found");
+    }
+});
+
+app.patch("/user" , async (req,res)=>{
+    const userId = req.body.userId;
+    const data= req.data;
+    try{
+        const user = await User.findByIdAndUpdate(userId,{name : "gautam gambhir" , email : "gautam@gmail.com"},{runValidators:true});
+        res.send(" Yes data is updated Successfully ");
+    }catch(err){
+        app.status(404).send("User not found");
+    }
+});
 
 connectDB()
 .then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,30 @@
-const mongoose=require("mongoose");
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,require:true
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    age: {
+        type: Number,
+        required: true, // Optional: Ensures age is always provided
+        validate: {
+            validator: function (value) {
+                return value >= 18 && value <= 65;  // Ensuring age is between 18 and 65
+            },
+            message: "User age must be between 18 and 65",
+        },
     },
     email: {
-        type:String,require:true
-    }
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+},{
+    timestamps : true,
 });
 
-const User=mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema);
 module.exports = User;
